4ae5b990e0fc536ef3ff31953ddf4134f4e945e3
cmoreside <cmoreside@DESKTOP-7LK8EKP>
2017-01-22 19:27:32 -0700

    Add project files.

---
+++ CodeMovement.EbcdicCompare.DataAccess/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeMovement.EbcdicCompare.DataAccess
+{
+    public class Class1
+    {
+    }
+}
